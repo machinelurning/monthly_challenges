1faba7bde96bf6bcaeb6f3a9e6d52524ddccc104
IK Manding <github@ianmanding.net>
2023-06-02 11:23:55 +0800

    return HTML

--- challenges/views.py
+++ challenges/views.py
@@ -36,6 +36,7 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
+        response_data = f"<h1>{challenge_text}</h1>"
     except KeyError:
-        return HttpResponseNotFound("This month is not supported!")
-    return HttpResponse(challenge_text)
+        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
+    return HttpResponse(response_data)
