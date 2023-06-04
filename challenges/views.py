e4c6e86b4c94827d84bb591b6ce141519f742ae4
IK Manding <github@ianmanding.net>
2023-06-04 11:41:54 +0800

    templates

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 # Create your views here.
 
@@ -20,6 +21,25 @@ monthly_challenges = {
 }
 
 
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+
+        list_items += f'<li><a href="{month_path}"><h1>{capitalized_month}</h1></li>'
+
+    response_data = f"""
+        <ul>
+        {list_items}
+        </ul>
+    """
+
+    return HttpResponse(response_data)
+
+
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
 
@@ -36,7 +56,7 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data = f"<h1>{challenge_text}</h1>"
+        response_data = render_to_string("challenges/challenge.html")
+        return HttpResponse(response_data)
     except KeyError:
         return HttpResponseNotFound("<h1>This month is not supported!</h1>")
-    return HttpResponse(response_data)
