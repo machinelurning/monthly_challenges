d2313cb41b226f164390db67b2c8aa91d29a9270
IK Manding <github@ianmanding.net>
2023-06-05 12:27:20 +0800

    404 templates

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 from django.template.loader import render_to_string
 
@@ -65,4 +65,6 @@ def monthly_challenge(request, month):
             },
         )
     except KeyError:
-        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
+        # raise Http404()
+        response_data = render_to_string("404.html")
+        return Http404(response_data)
